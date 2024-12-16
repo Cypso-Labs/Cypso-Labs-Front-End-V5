06abfa0bea4b64fe7ea6c358e05050ca79ddea6d
Nadula494 <nadula@cypsolabs.com>
2024-12-16 16:04:29 +0530

    change the font to Rajdhani

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "@fontsource/rajdhani/500.css"; // Rajdhani Font Imported
 import "./globals.css";
-import  NavBar  from "./components/modals/NavBar";
+import NavBar from "./components/modals/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +28,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{ fontFamily: "Rajdhani, sans-serif" }} // Apply Rajdhani font globally
       >
-        <NavBar/>
+        <NavBar />
         {children}
       </body>
     </html>
