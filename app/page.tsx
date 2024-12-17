b21d2fbea96a99d43cec5bfb5fb7c2780decc798
tharumadhushan <tharushamadhushan555@gmail.com>
2024-12-17 11:17:31 +0530

    create hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-
+import Page from "../app/home/page"; // Ensure the export is a default export in 'page'
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Page />
+    </div>
   );
 }
