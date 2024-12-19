4d3ff0d2592df70aeb85949d7c7b0e926e9fd5c1
Dulmika <dulmika@cypsolabs.com>
2024-12-19 11:33:45 +0530

    Create a workflow for publish the project in GitHub pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/cypso-labs-front-end-v5",
+  output: "export",  // <=== enables static exports
   /* config options here */
 };
 
