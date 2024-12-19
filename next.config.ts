0dd7e8b4a1872e571686ce37ee1b02779886600b
Dulmika <dulmika@cypsolabs.com>
2024-12-19 12:28:35 +0530

    remove all the GitHub workflows

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/cypso-labs-front-end-v5",
-  output: "export",  // <=== enables static exports
   /* config options here */
 };
 
